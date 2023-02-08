09a57bea9dfabc67f7068f047c6134fd622214d7
Maryne C <maryne.cuvelier@gmail.com>
2023-02-08 08:35:00 +0100

    ajout collapse page a propos

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import arrow_icon from "../assets/vector.png";
+
+
+function Collapse(props) {
+    const [Open, setOpen] = useState(false);
+    return (
+        <div className="dropdown">
+            <div className="dropdown_header">
+                {props.title}
+                <div>
+                    {Open ? (
+                        <button onClick={() => setOpen(false)}>
+                            <img
+                                className="arrow arrow--up"
+                                src={arrow_icon}
+                                alt="bouton"
+                            />
+                        </button>
+                    ) : (
+                        <button onClick={() => setOpen(true)}>
+                            <img
+                                className="arrow arrow--down"
+                                src={arrow_icon}
+                                alt="bouton"
+                            />
+                        </button>
+                    )}
+                </div>
+            </div>
+            <div className={Open ? "content" : "content--hidden"}>
+                {props.children}
+            </div>
+        </div>
+    );
+}
+
+export default Collapse;
--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -2,12 +2,35 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Banner from '../components/Banner_apropos';
+import Collapse from '../components/Collapse';
 
 function Apropos() {
   return ( 
     <React.Fragment>
       <Header/>
       <Banner/>
+      <section className="about">
+        <Collapse title="Fiabilité">
+          <span>
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
+          </span>
+        </Collapse>
+        <Collapse title="Respect">
+          <span>
+            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
+          </span>
+        </Collapse>
+        <Collapse title="Service">
+          <span>
+            Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
+          </span>
+        </Collapse>
+        <Collapse title="Sécurité">
+          <span>
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
+          </span>
+        </Collapse>
+      </section>
       <Footer/>
     </React.Fragment>
   )
