c471b286d84173c99ccdb897d249bbc623fc0d8c
Maryne C <maryne.cuvelier@gmail.com>
2023-02-09 09:33:01 +0100

    correction style

--- src/pages/Logements.js
+++ src/pages/Logements.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Logement() {
-    return (
-      <React.Fragment>
-        <Header/>
-        <Footer/>
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <Header/>
+      <Footer/>
+    </React.Fragment>
+  )
+}
   
   export default Logement;
   
