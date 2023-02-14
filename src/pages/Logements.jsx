08dd9644c30388684caeacf11b012ee49c4933fe
Maryne C <maryne.cuvelier@gmail.com>
2023-02-14 18:10:51 +0100

    style titre des logements

--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -8,48 +8,45 @@ import HostName from '../components/HostName'
 import Tags from '../components/Tags'
 import Rating from '../components/Rating'
 
-function Accomodation() {
+function Logement() {
   const { id } = useParams(window.location.href)
-  console.log("Voilà l'appartement numéro " + id)
 
   const logement_Data = Data.find((object) => object.id === id)
-  if (!logement_Data) return <NotFound />
+  if (!logement_Data) return <NotFound/>
 
   return (
-    <main className="accommodation">
+    <main className="logement">
       <Carrousel slideshow={logement_Data.pictures} />
-      <section className="accommodation_top">
-        <div className="accommodation_details_left">
-          <h1 className="accommodation_title">{logement_Data.title}</h1>
-          <h2 className="accommodation_location">{logement_Data.location}</h2>
-          <div className="accommodation_tags">
+      <section className="logement_top">
+        <div className="logement_details_left">
+          <h1 className="logement_title">{logement_Data.title}</h1>
+          <h2 className="logement_location">{logement_Data.location}</h2>
+          <div className="logement_tags">
             <Tags tags={logement_Data.tags} />
           </div>
         </div>
 
-        <div className="accommodation_details_right">
-          <div className="accommodation_hostCard">
+        <div className="logement_details_right">
+          <div className="logement_hostCard">
             <Rating rating={logement_Data.rating} />
             <HostName host={logement_Data.host} />
           </div>
         </div>
       </section>
-      <section className="about">
+      <section className="about_logement">
         <Collapse title="Description">
           <span>
             {logement_Data.description}
           </span>
         </Collapse>
         <Collapse title="Équipements">
-          <ul>
-            <li className='collapse_logement'>
-              {logement_Data.equipments}
-            </li>
-          </ul>
+          <span className="about_logement_equipments">
+            {logement_Data.equipments}
+          </span>
         </Collapse>
       </section>
     </main>
   )
 }
 
-export default Accomodation
+export default Logement
